0504076867679b9fb95ee5c33cada7e7cb7843b1
Arshia Mahajan <arshiamahajan2006@gmail.com>
2025-01-12 23:17:01 +0530

    Update ShortCard.jsx

--- ShortCard.jsx
+++ ShortCard.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
-const ShortCard = ({ tasks }) => {
+const ShortCard = ({ tasks, onEdit, onDelete }) => {
   return (
-    <div className="short-card-container">
+    <div className="flex flex-wrap gap-4">
       {tasks.map((task, index) => (
-        <div key={index} className="short-card">
-          <h3>Task {index + 1}</h3>
-          <p>{task}</p>
+        <div key={index} className="task-card w-64 bg-purple-100 p-4 rounded-lg shadow-md">
+          {task.map((taskItem, taskIndex) => (
+            <div key={taskIndex} className="task-item flex justify-between items-center">
+              <p className="flex-grow text-xl font-medium"> 
+                {taskItem}
+              </p>
+              <div className="flex space-x-3">
+                <button className="text-blue-600 hover:text-blue-800 font-bold text-lg" onClick={() => onEdit(index, taskIndex, taskItem)} >
+                  Edit</button>
+                <button className="text-red-600 hover:text-red-800 font-bold text-lg" onClick={() => onDelete(index, taskIndex)}>Delete</button>
+                 </div>
+            </div>
+          ))}
         </div>
       ))}
     </div>
   );
 };
+
 export default ShortCard;
