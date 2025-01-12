039dc3a7dfdd880f491e915fabc89a6a8af6b1e3
Arshia Mahajan <arshiamahajan2006@gmail.com>
2025-01-12 23:10:58 +0530

    Update TaskFile.jsx

--- TaskFile.jsx
+++ TaskFile.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
-const TaskFile = ({taskInput,handleInputChange,addTaskToList, addTasksToMainList,taskList,}) => {
+const TaskFile = ({ taskInput, handleInputChange, addTaskToList, addTasksToMainList, taskList }) => {
   return (
-    <div className="task-file">
-      <h2>Add Your Tasks</h2>
-      <input type="text" value={taskInput} onChange={handleInputChange} placeholder="Enter task"/>
-      <button onClick={addTaskToList}>Add Task</button>
+    <div className="flex flex-col justify-center items-center mt-8">
+      <h2 className="text-xl font-semibold mb-4">Add Your Tasks</h2>
+      <input
+        type="text"
+        value={taskInput}
+        onChange={handleInputChange}
+        className="border-2 border-purple-500 p-2 rounded-md mb-4"
+        placeholder="Enter task description"
+      />
+      <button
+        onClick={addTaskToList}
+        className="bg-purple-500 text-white px-6 py-2 rounded-lg mb-4 hover:bg-purple-600">
+        Add Task
+      </button>
       <div className="task-list">
-        {taskList.map((task, index) => (
-          <div key={index} className="task-item">
+        {taskList.length > 0 && taskList.map((task, index) => (
+          <div key={index} className="mb-2 p-3 border-2 border-purple-300 rounded-md">
             {task}
           </div>
         ))}
       </div>
-      <button onClick={addTasksToMainList} className="final-add-btn">Add All Tasks</button>
+      {taskList.length > 0 && (
+        <button
+          onClick={addTasksToMainList}
+          className="bg-purple-500 text-white px-6 py-3 rounded-lg mt-4 hover:bg-purple-600"
+        >Add All Task</button>
+      )}
     </div>
   );
 };
